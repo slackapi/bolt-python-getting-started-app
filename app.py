20d3e12f4eff7ea54bc270aad4f0030547911651
Sarah Jiang <sarahjiang@slack-corp.com>
2021-08-04 10:58:35 -0700

    Add note

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import os
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
+# This sample slack application uses SocketMode
+# For the companion getting started setup guide, 
+# see: https://slack.dev/bolt-python/tutorial/getting-started 
+
 # Initializes your app with your bot token and signing secret
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
